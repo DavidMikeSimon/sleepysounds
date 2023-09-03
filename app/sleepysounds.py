6327337c8cf72b54c8457442a0da01b448d1f300
David Simon <david.simon@color.com>
2023-09-03 10:16:54 -0400

    Autoformat

--- app/sleepysounds.py
+++ app/sleepysounds.py
@@ -12,22 +12,23 @@ import config
 playback = Playback()
 playback.loop_at_end(True)
 
-sound_dictionary = { os.path.splitext(fn)[0]: fn for fn in os.listdir(config.sounds_dir) }
+sound_dictionary = {os.path.splitext(fn)[0]: fn for fn in os.listdir(config.sounds_dir)}
 sound_dictionary_keys = sorted(list(sound_dictionary.keys()))
 sound_dictionary_index = 0
 
 device_name = f"{config.name_prefix} Sleepy Sounds"
 
-device_info = DeviceInfo(
-    name=device_name,
-    identifiers=device_name
+device_info = DeviceInfo(name=device_name, identifiers=device_name)
+
+mqtt_settings = Settings.MQTT(
+    host=config.mqtt_host, username=config.username, password=config.password
 )
 
-mqtt_settings = Settings.MQTT(host=config.mqtt_host, username=config.username, password=config.password)
 
 def load_current_sound():
     fn = sound_dictionary[sound_dictionary_keys[sound_dictionary_index]]
     playback.load_file(os.path.join(config.sounds_dir, fn))
+
 
 def switch_change_request(client, user_data, message):
     payload = message.payload.decode()
@@ -40,15 +41,15 @@ def switch_change_request(client, user_data, message):
     else:
         print(f"Unknown payload {payload} in switch_change_request")
 
+
 playing_switch_info = SwitchInfo(
-    name=f"{device_name} Playing",
-    unique_id="{device_name}_playing",
-    device=device_info
+    name=f"{device_name} Playing", unique_id="{device_name}_playing", device=device_info
 )
 playing_switch_settings = Settings(mqtt=mqtt_settings, entity=playing_switch_info)
 playing_switch = Switch(playing_switch_settings, switch_change_request)
 
 playing_switch.off()
+
 
 def next_button_request(client, user_data, message):
     global sound_dictionary_index
@@ -57,10 +58,11 @@ def next_button_request(client, user_data, message):
     playback.play()
     playing_switch.on()
 
+
 next_button_info = ButtonInfo(
     name=f"{device_name} Next Sound",
     unique_id="{device_name}_playing",
-    device=device_info
+    device=device_info,
 )
 next_button_settings = Settings(mqtt=mqtt_settings, entity=next_button_info)
 next_button = Button(next_button_settings, next_button_request)
@@ -68,11 +70,13 @@ next_button = Button(next_button_settings, next_button_request)
 # FIXME: This should happen automatically
 next_button.write_config()
 
+
 def main():
     load_current_sound()
 
     while True:
         time.sleep(10)
+
 
 if __name__ == "__main__":
     main()
