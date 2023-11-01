e967cd2f65dceae14d4bc746a33d3b077470f438
David Simon <david.simon@color.com>
2023-11-01 11:40:51 -0400

    Configure ALSA with env vars

--- app/config.py
+++ app/config.py
@@ -6,3 +6,6 @@ sounds_dir = os.environ['SOUNDS_DIR']
 mqtt_host = os.environ['MQTT_HOST']
 username = os.environ['MQTT_USER']
 password = os.environ['MQTT_PASS']
+
+alsa_card_num = os.environ['ALSA_CARD_NUM']
+alsa_control = os.environ['ALSA_CONTROL']
